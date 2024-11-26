26f56a764ce535e8d6d906fbc7a5d509e4695961
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-26 20:20:06 +0100

    Add categories to homepage header

--- src/main/java/io/github/wkktoria/pagenook/controller/frontend/HomeServlet.java
+++ src/main/java/io/github/wkktoria/pagenook/controller/frontend/HomeServlet.java
@@ -1,5 +1,7 @@
 package io.github.wkktoria.pagenook.controller.frontend;
 
+import io.github.wkktoria.pagenook.dao.CategoryDAO;
+import io.github.wkktoria.pagenook.entity.Category;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,11 +10,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("")
-public class HomePageServlet extends HttpServlet {
+public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        CategoryDAO categoryDAO = new CategoryDAO();
+        List<Category> listCategory = categoryDAO.listAll();
+        request.setAttribute("listCategory", listCategory);
+
         final String homepage = "frontend/index.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(homepage);
         dispatcher.forward(request, response);
--- src/main/webapp/frontend/header.jsp
+++ src/main/webapp/frontend/header.jsp
@@ -1,8 +1,10 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <div style="text-align: center; margin-bottom: 32px">
     <div>
         <img style="vertical-align:middle" src="images/PageNookLogo.svg" width="200" height="100" alt="PageNook Logo"/>
         <span style="vertical-align:middle; font-weight: bold; font-size: 32px">PageNook</span>
     </div>
+
     <div>
         <label>
             <input type="text" name="keyword" size="50"/>
@@ -12,5 +14,13 @@
         <a href="#">Sign In</a> |
         <a href="#">Register</a> |
         <a href="#">Cart</a>
+    </div>
+
+    <div style="padding-top: 16px">
+        <c:forEach var="category" items="${listCategory}" varStatus="status">
+            <a href="view_category?id=${category.id}">
+                <c:out value="${category.name}"/>
+            </a><c:if test="${not status.last}"> | </c:if>
+        </c:forEach>
     </div>
 </div>
