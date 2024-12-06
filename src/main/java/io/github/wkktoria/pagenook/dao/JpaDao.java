e74a2636480aa6538273a0ced6783e028948fad0
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-12-06 23:41:17 +0100

    Refactor code
    Replace HibernateUtil class with EntityManagerFactory in the JpaDao.

--- src/main/java/io/github/wkktoria/pagenook/dao/JpaDao.java
+++ src/main/java/io/github/wkktoria/pagenook/dao/JpaDao.java
@@ -1,133 +1,140 @@
 package io.github.wkktoria.pagenook.dao;
 
-import io.github.wkktoria.pagenook.util.HibernateUtil;
-import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import jakarta.persistence.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class JpaDao<E> {
+    private static final EntityManagerFactory entityManagerFactory;
+
+    static {
+        entityManagerFactory = Persistence.createEntityManagerFactory("PageNook");
+    }
+
     public E create(E entity) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        session.persist(entity);
-        session.flush();
-        session.refresh(entity);
+        entityManager.persist(entity);
+        entityManager.flush();
+        entityManager.refresh(entity);
 
-        transaction.commit();
-        session.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
         return entity;
     }
 
     public E update(E entity) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        session.merge(entity);
+        entity = entityManager.merge(entity);
 
-        transaction.commit();
-        session.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
         return entity;
     }
 
     public E find(Class<E> type, Object id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-        E entity = session.get(type, id);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        E entity = entityManager.find(type, id);
 
         if (entity != null) {
-            session.refresh(entity);
+            entityManager.refresh(entity);
         }
 
-        session.close();
+        entityManager.close();
 
         return entity;
     }
 
     public void delete(Class<E> type, Object id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        Object reference = session.getReference(type, id);
-        session.remove(reference);
+        Object reference = entityManager.getReference(type, id);
+        entityManager.remove(reference);
 
-        transaction.commit();
-        session.close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     public List<E> findWithNamedQuery(final String queryName) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Query query = session.createNamedQuery(queryName, Entity.class);
-
+        Query query = entityManager.createNamedQuery(queryName);
         List<E> result = query.getResultList();
 
-        session.close();
+        entityManager.close();
 
         return result;
     }
 
     public List<E> findWithNamedQuery(final String queryName, final String paramName, Object paramValue) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Query query = session.createNamedQuery(queryName, Entity.class);
+        Query query = entityManager.createNamedQuery(queryName);
         query.setParameter(paramName, paramValue);
 
         List<E> result = query.getResultList();
 
-        session.close();
+        entityManager.close();
 
         return result;
     }
 
     public List<E> findWithNamedQuery(final String queryName, final Map<String, Object> parameters) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Query query = session.createNamedQuery(queryName, Entity.class);
+        Query query = entityManager.createNamedQuery(queryName);
 
         Set<Map.Entry<String, Object>> setParameters = parameters.entrySet();
-
         for (Map.Entry<String, Object> entry : setParameters) {
             query.setParameter(entry.getKey(), entry.getValue());
         }
 
         List<E> result = query.getResultList();
 
-        session.close();
+        entityManager.close();
 
         return result;
     }
 
     public List<E> findWithNamedQuery(final String queryName, int first, int max) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Query query = session.createNamedQuery(queryName, Entity.class);
+        Query query = entityManager.createNamedQuery(queryName);
         query.setFirstResult(first);
         query.setMaxResults(max);
 
         List<E> result = query.getResultList();
 
-        session.close();
+        entityManager.close();
 
         return result;
     }
 
     public long countWithNamedQuery(final String queryName) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Query query = session.createNamedQuery(queryName, Entity.class);
+        Query query = entityManager.createNamedQuery(queryName);
+        long result = query.getResultList().size();
 
-        long result = (long) query.getSingleResult();
-
-        session.close();
+        entityManager.close();
 
         return result;
+    }
+
+    public void close() {
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
     }
 }
